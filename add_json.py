25153a058d013ea2468fd30ab8fa922f2cb3bdf4
mrmiguez <r.m.miguez@gmail.com>
2020-02-05 05:20:37 -0800

    scripting

--- add_json.py
+++ add_json.py
@@ -1,13 +1,29 @@
-import json
-import sys
+#!/usr/bin/env python3
 
-with open(sys.argv[2], 'w') as json_out:
-    doc_json = json.load(json_out)
-    add_file = open(sys.argv[1])
-    add_json = json.load(add_file)
-    for rec in add_json:
-        doc_json.append(rec)
-    json.dump(doc_json, json_out)
-    json_out.close()
-    add_file.close()
+import sys
+import json
+import shutil
+import argparse
+
+
+def add_json(source, target):
+    shutil.move(target, target + '.bak')
+    with open(target, 'w') as json_out:
+        target_file = open(target + '.bak')
+        target_json = json.load(target_file)
+        source_file = open(source)
+        source_json = json.load(source_file)
+        for rec in source_json:
+            target_json.append(rec)
+        json.dump(target_json, json_out)
+        target_file.close()
+        source_file.close()
+        
+
+if __name__ == '__main__':
+    p = argparse.ArgumentParser()
+    p.add_argument('source', help='JSON document with records to add')
+    p.add_argument('destination', help='JSON document to append records to')
+    args = p.parse_args()
+    add_json(args.source, args.destination)
     
